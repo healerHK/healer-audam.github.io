7c4f4d23cee2d8792629e4f8ff7a80eabf11380e
Healer HK <82294908+healerHK@users.noreply.github.com>
2022-03-07 18:59:05 +0700

    1

---
+++ config.js
@@ -0,0 +1,12 @@
+const nameGirl = 'Em';
+const giftUrl = 'https://www.facebook.com/profile.php?id=100009509504790';
+const eventName = 'Chúc Mừng 8-3';
+const titleCard = 'Tặng Ấu Dâm';
+const contentCard = 'Chúc bạn chẻ ngày 8/3 tràn ngập niềm vui và những nụ cười. Ăn ít lại và vận động nhiều hơn cho nó healthy, hy vọng không dính F0 :D '
+// phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
+// Cần hỗ trợ hãy liên hệ: 
+// Mr-Nam http://facebook.com/nam.nodemy
+// Các bạn muốn học lập trình thì tham gia Nhóm zalo tự học lập trình nhé: https://zalo.me/g/yhdkef092
+const giftImage = 'hot-girl.png';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
